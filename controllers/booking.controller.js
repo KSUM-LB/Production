f11f66434987a33f3e63c49768bed399ba5b5961
KarimDeaibes <deaibes.karim@outlook.com>
2022-03-09 14:57:48 +0200

    valdiating data in booking

--- controllers/booking.controller.js
+++ controllers/booking.controller.js
@@ -3,6 +3,8 @@ const models = require("../models");
 
 // -- Create Booking
 exports.createBooking = (req, res) => {
+  const inDate = req.body.checkIn.split("-");
+  const outDate = req.body.checkOut.split("-");
   const booking = {
     manuel: req.body.manuel,
     status: true,
@@ -13,19 +15,37 @@ exports.createBooking = (req, res) => {
     nbOfTravellers: req.body.nbOfTravellers,
     nbOfRooms: req.body.nbOfRooms,
     nbOfTables: req.body.nbOfTables,
-    checkIn: req.body.checkIn,
-    checkOut: req.body.checkOut,
+    checkIn: new Date(inDate[0], inDate[1] - 1, inDate[2]),
+    checkOut: new Date(outDate[0], outDate[1] - 1, outDate[2]),
     price: req.body.price,
     couponId: req.body.couponId,
     total: req.body.total,
   };
   console.log(booking);
   // -- Validating data passed in request body
-  //   const bookig_schema = {
-  //     fullName: { type: "string", optional: false },
-  //     email: { type: "string", optional: false },
-  //     password: { type: "string", optional: false },
-  //     phoneNb: { type: "string", optional: true },
-  //   };
-  res.status(200).json({ message: "success" });
+  const booking_schema = {
+    manuel: { type: "boolean", optional: false },
+    status: { type: "boolean", optional: true, default: true },
+    CC: { type: "boolean", optional: false },
+    payed: { type: "boolean", optional: false },
+    userId: { type: "number", optional: false },
+    nbOfTravellers: { type: "number", optional: false },
+    nbOfRooms: { type: "number", optional: false },
+    nbOfTables: { type: "number", optional: false },
+    checkIn: { type: "date", optional: false },
+    checkOut: { type: "date", optional: false },
+    price: { type: "number", optional: false },
+    couponId: { type: "number", optional: true },
+    total: { type: "number", optional: false },
+  };
+  const v = new Validator();
+  const validationResponse = v.validate(booking, booking_schema);
+  if (validationResponse != true) {
+    return res.status(406).json({
+      message: "Error in Data format",
+      error: validationResponse,
+    });
+  } else {
+    res.status(200).json({ message: "success" });
+  }
 };
