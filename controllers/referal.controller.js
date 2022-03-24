463dd635eac0c48485c43f0ac19edc67eb29bee6
KarimDeaibes <deaibes.karim@outlook.com>
2022-03-24 07:28:07 +0200

    added error handling in referals

--- controllers/referal.controller.js
+++ controllers/referal.controller.js
@@ -1,12 +1,28 @@
 const models = require("./../models");
+const Validator = require("fastest-validator");
 
 // -- Create Referal
 exports.createReferal = (req, res) => {
-  models.Referal.create({ referalName: req.body.referal, count: 0 })
-    .then((referal) => {
-      res.status(200).json({ message: "success", referal: referal });
-    })
-    .catch((err) => res.status(500).json({ message: "Server Error" }));
+  const referal = { referalName: req.body.referalName, count: 0 };
+  const referal_schema = {
+    referalName: { type: "string", optional: false },
+    count: { type: "number", optional: false },
+  };
+  const v = new Validator();
+  const vValidation = v.validate(referal, referal_schema);
+  if (vValidation != true) {
+    headerRes = false;
+    return res.status(406).json({
+      message: "Error in referal data",
+      error: vValidation,
+    });
+  } else {
+    models.Referal.create(referal)
+      .then((referal) => {
+        res.status(200).json({ message: "success", referal: referal });
+      })
+      .catch((err) => res.status(500).json({ message: "Server Error" }));
+  }
 };
 
 // -- Get Referals
